efae4dfe9d3e7501a9c149139761c7d7b1e66d88
Frank Jung <frankhjung@linux.com>
2022-08-16 23:59:37 +1000

    added gitlab pipeline

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject scrapbook "0.1.0-SNAPSHOT"
   :description "Clojure Code Scraps"
-  :url "http://example.com/FIXME"
+  :url "https://gitlab.com/frankhjung1/clojure-scrapbook"
   :license {:name "GNU General Public License (GPLv3)"
             :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
