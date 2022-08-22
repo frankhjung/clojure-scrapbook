147609bfee70e722c7c66b1640c4e92c6549fd44
Frank Jung <frankhjung@linux.com>
2022-08-22 15:13:02 +1000

    read file and return persistent vector

--- src/scrapbook/files.clj
+++ src/scrapbook/files.clj
@@ -5,13 +5,12 @@
   (doseq [l (line-seq r)]
     (println l)))
 
-(defn print_words "Get English words from 3-9 characters long."
+(defn get_words* "Get English words from 3-9 characters long."
   [file]
   (with-open [reader (io/reader file)]
-    (doseq [w (line-seq reader)]
-      (->
-       (re-find #"[a-z]{3,9}" w)
-       (println)))))
+    (reduce conj []
+            (filter (fn [x] (re-find #"[a-z]{3,9}" x))
+                    (line-seq reader)))))
 
 (defn get_words "Get English words from 3-9 characters long."
   [file]
